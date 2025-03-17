8afae3d8ba45cdc2a1fcfb0a1a269f5d9cb37fe8
i.lukyanychev <i.lukyanychev@edu.centraluniversity.ru>
2025-03-17 15:46:20 +0300

    initial commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '14.0'
+
+target 'SimpleTasks' do
+  use_frameworks!
+
+  pod 'SnapKit', '~> 5.7.1'
+
+end
---
+++ SimpleTasks/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTasks
+//
+//  Created by Иван Лукъянычев on 17.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
